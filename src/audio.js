b0dffc86659d100915bc13abef7d3636168a83e4
Martin Liberg <martin.liberg@m.co>
2019-09-22 20:07:25 +0200

    Refactor a littel

--- src/audio.js
+++ src/audio.js
@@ -3,32 +3,32 @@ const detectPitch = Pitchfinder.AMDF();
 
 let lastFrequency = null;
 
-exports.onFrequencyChange = f =>
-  navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-    const context = new window.AudioContext();
-    const analyser = context.createAnalyser();
-    const processor = context.createScriptProcessor(4096, 1, 1);
-    const node = context.createMediaStreamSource(stream);
+exports.onFrequencyChange = async f => {
+  const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  const context = new window.AudioContext();
+  const analyser = context.createAnalyser();
+  const processor = context.createScriptProcessor(4096, 1, 1);
+  const audioSource = context.createMediaStreamSource(stream);
+  const process = event => {
+    const frequency = detectPitch(event.inputBuffer.getChannelData(0));
 
-    const process = event => {
-      const frequency = detectPitch(event.inputBuffer.getChannelData(0));
+    if (frequency !== lastFrequency) {
+      lastFrequency = frequency;
+      f(frequency);
+    }
+  };
 
-      if (frequency !== lastFrequency) {
-        lastFrequency = frequency;
-        f(frequency);
-      }
-    };
+  audioSource.connect(analyser);
+  analyser.connect(processor);
+  processor.connect(context.destination);
+  processor.addEventListener("audioprocess", process);
 
-    node.connect(analyser);
-    analyser.connect(processor);
-    processor.connect(context.destination);
-    processor.addEventListener("audioprocess", process);
-
-    return () => {
-      processor.removeEventListener("audioprocess", process);
-      processor.disconnect();
-      analyser.disconnect();
-      stream.getTracks().forEach(track => track.stop());
-      context.close();
-    };
-  });
+  return () => {
+    processor.removeEventListener("audioprocess", process);
+    stream.getTracks().forEach(track => track.stop());
+    processor.disconnect();
+    analyser.disconnect();
+    audioSource.disconnect();
+    context.close();
+  };
+};
